df22bd75aed288cc3fe2a1f19930507f8f7461cd
ErikHartman <erik.hartman@hotmail.com>
2021-12-25 19:46:44 +0100

    update

--- peptide_networks/peptide_graphs/PeptideNetwork.py
+++ peptide_networks/peptide_graphs/PeptideNetwork.py
@@ -8,8 +8,7 @@ import re
 
 
 class PeptideNetwork():
-    edge_list = pd.DataFrame()
-    
+       
     def __init__(self,edge_list):
         """
         Creates network from given edge list
@@ -19,25 +18,40 @@ class PeptideNetwork():
         """
         self.edge_list = edge_list
         self.edge_list['weight'] = 1/(1+edge_list['distance'])
+        
+    def create_network(self):
+        """ Create network using the given edge list. 
+        Source = 'from'
+        Target = 'to'
+        Edge attribute = 'weight'
+        """
         self.G = nx.from_pandas_edgelist(
-            edge_list,
+            self.edge_list,
             source='from',
             target='to',
             edge_attr="weight")
-        
-        
+                
     def get_communitiess(self):
+        """ Returns a communities object using the Girvan-Newman method.
+        """
         return community.girvan_newman(self.G)
     
     def subset_edge_list_with_threshold(self,threshold):
+        """ 
+        Subsets the edge list based on distance threshold
+        """
         subset_edge_list = self.edge_list[ (self.edge_list['distance'] <= threshold)]
         self.edge_list = subset_edge_list
     
     def subset_edge_list_with_protein(self, proteins):
+        """
+        Subsets the edge list based on wanted proteins
+        """
         subset_edge_list = self.edge_list[self.edge_list['accession'].isin(proteins)]
         self.edge_list = subset_edge_list
         
-    def print_edge_histogram(self,save_path):
+    def print_distance_histogram(self,save_path):
+        """ Saves a distance histogram to the given save path """
         x = self.edge_list['distance']
         plt.hist(x, density=True, bins=100)
         plt.title(f'Distance histogram {save_path}')
@@ -46,16 +60,17 @@ class PeptideNetwork():
         
         
     def plot_network(self, save_path):
-        """ want to create color mapper based on accession value"""
+        """ Plots the network """
         pos = nx.spring_layout(self.G, weight="weight")
-        degree_centrality = nx.degree_centrality(G)
+        degree_centrality = nx.degree_centrality(self.G)
         degree_centrality_color = np.fromiter(degree_centrality.values(), float)
         nx.draw(self.G, pos=pos, node_size=20, with_labels=False, width=1, alpha=0.6, node_color=degree_centrality_color)
-        plt.title(f'Network {save_path}')
+        plt.title(f'Network')
         plt.savefig(f'{save_path}')
          
     def degree_analysis(self, save_path):
-        degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
+        """ Saves simple degree analysis to the given save path """
+        degree_sequence = sorted([d for n, d in self.G.degree()], reverse=True)
         fig, ax= plt.subplots(2,1, figsize=(8, 8))
         ax[0].plot(degree_sequence, "b-", marker="o")
         ax[0].set_title("Degree Rank Plot")
@@ -67,25 +82,21 @@ class PeptideNetwork():
         ax[1].set_ylabel("# of Nodes")
         fig.tight_layout()
         save_path = save_path.split('/')[-2] + '_' + re.sub('\D', '', save_path)
-        plt.savefig(f'findings/peptide_graphs/degree_{save_path}.jpg')
+        plt.savefig(f'{save_path}')
 
-        
+# ------------------------------------------------------------------------------ #
 
-
-
-
-
-    
-    
 
 def main(args):
     filepath = args.filepath
     edge_list = pd.read_csv(filepath)
     proteins = ['HBB_HUMAN', 'FIBA_HUMAN']
     threshold = 6
-    
     G = PeptideNetwork(edge_list)
     G.subset_edge_list_with_protein(proteins)
+    G.subset_edge_list_with_threshold(threshold)
+    G.create_network()
+    G.plot_network('findings/peptide_graphs/network_blosum_34.jpg')
     
 
 if __name__ == "__main__":
